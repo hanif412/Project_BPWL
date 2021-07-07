80c55950e4de383ebc6bc84f5df5d782cad5f865
anwar2731 <85085697+anwar2731@users.noreply.github.com>
2021-07-07 23:52:08 +0700

    menambahkan method cek_login
    method ini digunakan  untuk mengecek login nya

--- application/models/v_mahasiswa.php
+++ application/models/v_mahasiswa.php
@@ -5,6 +5,10 @@ class v_Mahasiswa extends CI_Controller
     function get_data($table){
         return $this->db->get($table);
     }
+ 	function cek_login($table, $where)
+    {
+        return $this->db->get_where($table, $where);
+    }
 }
 
 ?>
